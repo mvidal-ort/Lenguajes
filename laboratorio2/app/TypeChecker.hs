74e404696588454b589e48cbf2c35e75f575d1fa
Vidal Servetto <d196916@net.in.iantel.com.uy>
2026-05-12 15:53:36 -0300

    labo2_3

--- laboratorio2/app/TypeChecker.hs
+++ laboratorio2/app/TypeChecker.hs
@@ -12,14 +12,32 @@ import Prelude hiding (fail)
 #endif
 
 typecheck :: Program -> Err ()
-typecheck p = do
-  checkProg emptyEnv p
+typecheck (PDefs defs) = do
+  env <- buildSig emptyEnv defs
+  checkProg env (PDefs defs)
+
+---- Construye el env, extrayendo los tipos de las funciones declaradas en el programa
+---- Se llama a esta funcion desde el arranque con typecheck, pasando un env vacío.
+buildSig :: Env -> [Def] -> Err Env
+buildSig env [] = return env
+buildSig env (d:ds) =
+  case d of
+    DFun t id args _ -> do --si la definicion es una función, extrae el tipo y lo agrega al env      
+      let argTypes = [ ty | ADecl ty _ <- args ]
+      env' <- updateFun env id (argTypes, t)
+      buildSig env' ds
 
 checkProg :: Env -> Program -> Err ()
-checkProg = undefined 
+checkProg env (PDefs defs) =
+  mapM_ (checkDef env) defs
 
+-- Para probar
 checkDef :: Env -> Def -> Err ()
-checkDef = undefined
+checkDef _ _ = return ()
+
+-- Supende temporal para poder probar y que no explote por undefined
+-- checkDef :: Env -> Def -> Err ()
+-- checkDef = undefined
 
 inferExp :: Env -> Exp -> Err Type
 inferExp = undefined
